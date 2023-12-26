16a667f276bf62beac10efe828304feef4c0834f
VikasKumar-19 <vk.pratap98@gmail.com>
2023-12-26 14:56:02 +0530

    creating nesting and sibling elements using core react

--- lecture_1/app.js
+++ lecture_1/app.js
@@ -7,7 +7,25 @@ const heading = React.createElement(
 );
 //React.createElement returns react element which is basically a javascript object which represents any html tag.
 
+//Creating Nesting and sibling elements using React:
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I'm an h1 tag</h1>
+ *          <h2>I'm an h2 tag</h2>
+ *      </div>
+ * </div>
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an h2 tag"),
+  ])
+);
+
 //creating a root so that it can render something inside it.
 //it is a part of react dom library.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // it will take react element and convert in actual html tag and put it inside (root) dom.
+root.render(parent); // it will take react element and convert in actual html tag and put it inside (root) dom.
