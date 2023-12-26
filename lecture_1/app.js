c9cb96f9824ef41204d2fe159aae04898b3146e4
VikasKumar-19 <vk.pratap98@gmail.com>
2023-12-26 15:15:56 +0530

    added comment for render method

--- lecture_1/app.js
+++ lecture_1/app.js
@@ -28,4 +28,5 @@ const parent = React.createElement(
 //creating a root so that it can render something inside it.
 //it is a part of react dom library.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // it will take react element and convert in actual html tag and put it inside (root) dom.
+root.render(parent); // it will take react element and convert in actual html tag and put it inside (root) dom. 
+//root.render() method will replace everything in the root tag.
